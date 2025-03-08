a5f30c2cd82e59336a00f0b97c734a7258fdb73b
ELmustasje <thomasbarth@live.no>
2025-03-08 14:49:50 +0100

    update 8.3.2025

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ export const Hero = () => {
         <h1 className={styles.title}>Hey, I'm Thomas</h1>
         <p className={styles.description}>
           I'm a computer science student at the University of Bergen, and board memeber at both FriByte and BSI-golf.
+          Winner of Labyrinten UiB (team programming contest), qualified to the final in Oslo.
         </p>
       </div>
       <div className={styles.topBlur} />
